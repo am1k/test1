c812f5a7bdd33b08f36cbbc01c9b5b0d50548db9
am1k <vitaliybog@yandex.ru>
2014-01-16 10:49:45 +0200

    Delete button-color.js

--- button-color.js
+++
@@ -1,9 +0,0 @@
-document.onclick = function(e) {
-var target = e && e.target || window.event.srcElement;
-
-var dataToggleClass = target.getAttribute('data-toggle-class');
-if (!dataToggleClass) return;
-
-var elem = document.getElementById(dataToggleClass);
-elem.style.display = elem.offsetHeight ? 'none' : 'block';
-}
